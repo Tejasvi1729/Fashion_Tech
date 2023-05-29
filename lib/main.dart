192974918e1790958f3b6093463e19e8c1ffec28
Tejasvi_1729 <tejasvi21@iitk.ac.in>
2023-05-29 09:54:40 +0530

    third commit on 29th morning

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'login_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttersample/login_page.dart';
+// import 'login_page.dart';
 // import 'sign_up.dart';
 import "package:get/get.dart";
-import 'auth_controller.dart';
-Future<void> main() async{
+// import 'auth_controller.dart';
+// import 'package:fluttersample/welcome_page.dart';
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value)=>Get.put(AuthController()));
+  // await Firebase.initializeApp().then((value)=>Get.put(AuthController()));
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
